5331d654226227ac4c22ff19113940b424768984
Alexin <alexindevs@gmail.com>
2024-01-02 23:25:06 +0100

    Create auth interface

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,8 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import * as schema from './user.interface';
+import UserModel from "./user.model";
+
+export class AuthService {
+    
+}
---
+++ src/auth/user.interface.ts
@@ -0,0 +1,67 @@
+interface UserAndTokens {
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    isAdmin: boolean;
+    isBlocked: boolean;
+    profileImage?: string | null;
+    otp?: string | null;
+    createdAt: Date;
+    updatedAt: Date;
+    refreshTokens?: RefreshToken[];
+  }
+
+  interface User {
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    isAdmin: boolean;
+    isBlocked: boolean;
+    profileImage?: string | null;
+    otp?: string | null;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+
+  interface UserWithoutId {
+    username: string;
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    isAdmin: boolean;
+    isBlocked: boolean;
+    profileImage?: string | null;
+    otp?: string | null;
+  }
+
+  interface UserWithoutPassword {
+    id: number;
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    isAdmin: boolean;
+    isBlocked: boolean;
+    profileImage?: string | null;
+    otp?: string | null;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+  
+  interface RefreshToken {
+    id: string;
+    userId: number;
+    token: string;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+  
+  export { User, UserWithoutPassword, UserWithoutId, UserAndTokens, RefreshToken };
+  
