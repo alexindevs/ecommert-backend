7c2f4ede4f8ff08ea51209fd86398d45d40a2528
Alexin <alexindevs@gmail.com>
2024-01-04 21:11:44 +0100

    Added Auth Middleware

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,15 +1,16 @@
 import express from 'express';
 import AuthController from './auth.controller';
+import  * as AuthMiddleware from './auth.middleware';
 
 const AuthRouter = express.Router();
 
 AuthRouter.post('/register', AuthController.registerUser);
 AuthRouter.post('/login', AuthController.loginUser);
 AuthRouter.post('/logout/:userId', AuthController.logoutUser);
-AuthRouter.get('/user/:userId', AuthController.getUserById);
-AuthRouter.put('/user/:userId', AuthController.updateUser);
-AuthRouter.delete('/user/:userId', AuthController.deleteUser);
-AuthRouter.put('/change-password/:userId', AuthController.changePassword);
+AuthRouter.get('/user/:userId', AuthMiddleware.tokenVerification, AuthController.getUserById);
+AuthRouter.put('/user/:userId', AuthMiddleware.tokenVerification, AuthController.updateUser);
+AuthRouter.delete('/user/:userId', AuthMiddleware.tokenVerification, AuthController.deleteUser);
+AuthRouter.put('/change-password/:userId', AuthMiddleware.tokenVerification, AuthController.changePassword);
 AuthRouter.post('/forgot-password', AuthController.forgotPassword);
 AuthRouter.put('/reset-password/:userId', AuthController.resetPassword);
 
