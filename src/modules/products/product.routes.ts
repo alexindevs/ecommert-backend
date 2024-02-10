3259c47feb05a4f9789decfb2c1f6deb6f508902
Alexin <alexindevs@gmail.com>
2024-02-10 11:51:53 +0100

    Update src/modules/products/product.routes.ts

--- src/modules/products/product.routes.ts
+++ src/modules/products/product.routes.ts
@@ -9,15 +9,19 @@ const ProductRoutes = Router();
 ProductRoutes.use(express.json());
 
 
-ProductRoutes.post("/products", tokenVerification, checkIfAdmin, upload.array('images', 5), productController.addProduct);
-ProductRoutes.delete("/products/:id",tokenVerification, checkIfAdmin,  productController.deleteProduct);
-ProductRoutes.get("/products/:id", productController.getProductById);
-ProductRoutes.get("/products/featured", productController.getFeaturedProducts);
-ProductRoutes.patch("/products/:id", productController.updateProduct);
-ProductRoutes.post("/products/:id/like", tokenVerification, productController.likeProduct);
-ProductRoutes.post("/products/:id/unlike",tokenVerification, productController.unlikeProduct);
-ProductRoutes.get("/products/:id/reviews", productController.fetchProductWithReviews);
-ProductRoutes.post("/products/:id/reviews",tokenVerification, productController.addReview);
-ProductRoutes.delete("/products/:id/reviews/:reviewId", tokenVerification, productController.deleteReview);
-ProductRoutes.get("/products", productController.getAllProducts);
-ProductRoutes.patch("/products/:id/reviews/:reviewId", tokenVerification, productController.updateReview);
+ProductRoutes.post("/", tokenVerification, checkIfAdmin, upload.array('images', 5), productController.addProduct);
+ProductRoutes.delete("/:id",tokenVerification, checkIfAdmin,  productController.deleteProduct);
+ProductRoutes.get("/:id", productController.getProductById);
+ProductRoutes.get("/featured", productController.getFeaturedProducts);
+ProductRoutes.post("/:id/feature", tokenVerification, checkIfAdmin, productController.makeFeatured);
+ProductRoutes.post("/:id/unfeature", tokenVerification, checkIfAdmin, productController.unmakeFeatured);
+ProductRoutes.patch("/:id", productController.updateProduct);
+ProductRoutes.post("/:id/like", tokenVerification, productController.likeProduct);
+ProductRoutes.post("/:id/unlike",tokenVerification, productController.unlikeProduct);
+ProductRoutes.get("/:id/reviews", productController.fetchProductWithReviews);
+ProductRoutes.post("/:id/reviews",tokenVerification, productController.addReview);
+ProductRoutes.delete("/:id/reviews/:reviewId", tokenVerification, productController.deleteReview);
+ProductRoutes.get("/", productController.getAllProducts);
+ProductRoutes.patch("/:id/reviews/:reviewId", tokenVerification, productController.updateReview);
+
+export default ProductRoutes;
