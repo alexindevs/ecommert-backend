9ddb25afd347e17f53898ea2ca28584d0d837cbb
Alexin <alexindevs@gmail.com>
2024-02-10 11:15:22 +0100

    Update src/modules/products/product.repository.ts

--- src/modules/products/product.repository.ts
+++ src/modules/products/product.repository.ts
@@ -1,4 +1,5 @@
-import { PrismaClient, Product } from "@prisma/client";
+import { PrismaClient, Product, Review  } from "@prisma/client";
+import ProductInput from "./product.interface";
 
 const prisma = new PrismaClient();
 
@@ -10,7 +11,7 @@ export default class ProductRepository {
         });
     }
 
-    async addProduct(product: Product): Promise<Product> {
+    async addProduct(product: ProductInput): Promise<Product> {
         return prisma.product.create({
             data: product,
         });
@@ -39,4 +40,53 @@ export default class ProductRepository {
             data: { likes: { increment: 1 } },
         });
     }
+
+    async unlikeProduct(id: number): Promise<Product | null> {
+        return prisma.product.update({
+            where: { id },
+            data: { likes: { decrement: 1 } },
+        });
+    }
+
+    async fetchProductWithReviews(id: number): Promise<Product | null> {
+        return prisma.product.findUnique({
+            where: { id },
+            include: { reviews: true },
+        });
+    }
+
+    async addReview(review: Review): Promise<Review> {
+        return prisma.review.create({
+            data: review,
+        });
+    }
+
+    async deleteReview(userId: number, productId: number): Promise<Review | null> {
+        return prisma.review.delete({
+            where: { 
+                userId_productId: {
+                    userId: userId,
+                    productId: productId
+                }
+            }
+        });
+    }
+
+    async updateReview(userId: number, productId: number, review: Partial<Review>): Promise<Review | null> {
+        return prisma.review.update({
+            where: {
+                userId_productId: {
+                    userId: userId,
+                    productId: productId
+                }
+            },
+            data: review,
+        });
+    }
+
+
+
+    
+
+    
 }
