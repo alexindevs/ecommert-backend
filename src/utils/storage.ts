a2fbfc1e3da3f2622e7b2896db7914a9392721ad
Alexin <alexindevs@gmail.com>
2024-03-01 16:31:35 +0100

    Tested Products Feature: src/utils/storage.ts

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const uploadDir = path.join(__dirname, '/uploads');
+    const uploadDir = path.join(__dirname, '../../uploads');
     cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
